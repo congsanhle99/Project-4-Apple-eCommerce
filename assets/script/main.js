961b7613dbbff4cbc46eecf4f8d2cc041fd86680
CongSanhLe <congsanhle1999@gmail.com>
2022-08-06 22:43:33 +0700

    lesson-4-5-6-7

--- assets/script/main.js
+++ assets/script/main.js
@@ -29,3 +29,55 @@ const slideShow = () => {
 };
 
 slideShow();
+
+// cube
+let x = 0;
+let y = 20;
+let z = 0;
+let bool = true;
+let interval;
+const cube = document.querySelector(".cube");
+
+const playPause = () => {
+  if (bool) {
+    interval = setInterval(() => {
+      cube.style.transform = `rotateY(${y++}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+    }, 100);
+  } else {
+    clearInterval(interval);
+  }
+};
+playPause();
+// hover control don't auto rotate
+document.querySelector(".controls").addEventListener("mouseover", () => {
+  bool = false;
+  playPause();
+});
+document.querySelector(".controls").addEventListener("mouseout", () => {
+  bool = true;
+  playPause();
+});
+// top
+document.querySelector(".top-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg) `;
+});
+// bottom
+document.querySelector(".bottom-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+//left
+document.querySelector(".left-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateY(${(y -= 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+});
+// right
+document.querySelector(".right-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateY(${(y += 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+});
+// top right
+document.querySelector(".top-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg) `;
+});
+// bottom left
+document.querySelector(".bottom-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg) `;
+});
